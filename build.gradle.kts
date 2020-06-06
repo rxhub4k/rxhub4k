6870d59e4340edaf92b8b1156dee30d0d1001b6a
nevinsjr <nevinsjr@gmail.com>
2020-06-06 15:00:50 -0600

    Add ktlint Support
    Add ktlint support via plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     java
     kotlin("jvm") version "1.3.72"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    id("com.dorongold.task-tree") version "1.5"
 }
 
 group = "com.nevinsjr.rxhub4k"
@@ -12,7 +14,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
