a180b9780d15eda0da66f6c75b80239db22aa6bc
nevinsjr <nevinsjr@gmail.com>
2020-06-19 08:18:34 -0600

    Add Apollo Client
    Add the client and a fledgling query.  This is a WIP,
    and queries will become cleaner with iteration.
    
    Still need to wire up schema download and code gen
    with local Gradle builds as well as CI.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("jvm") version "1.3.72"
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     id("com.dorongold.task-tree") version "1.5"
+    id("com.apollographql.apollo").version("2.2.0")
 }
 
 group = "com.nevinsjr.rxhub4k"
@@ -10,16 +11,25 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("junit", "junit", "4.12")
+    implementation("com.apollographql.apollo:apollo-runtime:2.2.0")
+    implementation("com.apollographql.apollo:apollo-coroutines-support:2.2.0")
+    implementation("com.apollographql.apollo:apollo-rx3-support:2.2.0")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
+apollo {
+    generateKotlinModels.set(true)
+}
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
